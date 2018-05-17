059a5924000d294a972e5cb2beb6874236dd6cab
Toni Sefton <21288605+Ariadne-S@users.noreply.github.com>
2018-05-17 22:52:23 +1000

    Let items be added to the to do list, via button and enter key.
    Basic formatting added, Delete Button added, but not functional. Input area resets after item has been added. Cannot add whitespace to list.

--- todolist.js
+++ todolist.js
@@ -1,7 +1,44 @@
+function isWhitespace(text) {
+    return !/\S/.test(text);
+}
+
+function dateFormat(d){
+    var monthNames = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+    var currDate = d.getDate();
+    var currMonth = d.getMonth();
+    var currYear = d.getFullYear();
+    return (currDate + " " + monthNames[currMonth] + " " + currYear);
+}
+
+function addToDo() {
+    var reminderInputValue = reminderInput.value; 
+    if (reminderInputValue != "" && !isWhitespace(reminderInputValue)) {
+        var todoItems = document.querySelector(".todo-items");
+        var item = document.createElement ("li");
+        var text = document.createElement("span");
+        var deleteButton =document.createElement("button");
+        text.innerText = reminderInputValue;
+        deleteButton.innerText = "X";
+        deleteButton.className = "delete-button";
+        item.appendChild(text);
+        item.appendChild(deleteButton);
+        todoItems.appendChild(item);
+        reminderInput.value = "";
+   }
+}
+
+var d = new Date();
+document.getElementById("date").innerText = dateFormat(d);
+
+var reminderInput = document.getElementById("reminder-input");
+
 var button = document.querySelector(".button");
-button.addEventListener("click", function(){
-    var todoItems = document.querySelector(".todo-items");
-    var item = document.createElement ("li");
-    item.innerText = "Go to Bed!";
-    todoItems.appendChild(item);
-})
+button.addEventListener("click", addToDo);
+reminderInput.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if(event.keyCode === 13){
+        addToDo();
+    }
+});
+
